c06fc64a96b61df23cc203cd632875ab7f839dd9
ArianFernandez <arian.fernandez.rodriguez@gmail.com>
2021-09-30 20:38:17 -0500

    return7

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-var history = require('connect-history-api-fallback');
+
 const serveStatic = require("serve-static")
 const path = require('path');
-var express = require('express');
-
-var app = express();
-app.use(history());
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
